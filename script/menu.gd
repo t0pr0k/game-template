7f27359d06b0172c60190eea44cced8a2f9ad91f
t0pr0k <84275926+t0pr0k@users.noreply.github.com>
2024-01-14 00:17:42 +0300

    подготовил для создание настроек
    подготовил сцену для настроек
    добавил функционал некоторых кнопок

---
+++ script/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://scene/settings.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
