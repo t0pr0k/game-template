3793d15ad5f953cb4cdd029ff747533642036ab6
t0pr0k <84275926+t0pr0k@users.noreply.github.com>
2024-03-17 23:51:14 +0300

    небольшие изменения

--- script/menu.gd
+++ script/menu.gd
@@ -6,10 +6,14 @@ func _ready():
 func _process(delta):
 	pass
 
+func _on_singleplayer_pressed():
+	pass
+
+func _on_multiplayer_pressed():
+	pass
 
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://scene/settings.tscn")
-
 
 func _on_exit_pressed():
 	get_tree().quit()
