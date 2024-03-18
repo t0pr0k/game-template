db23219e17d14366517be6b909ec2ec6479cd773
t0pr0k <84275926+t0pr0k@users.noreply.github.com>
2024-03-18 22:04:49 +0300

    подготовка к первому слиянию веток
    добавлены заглушки для начала игры
    отредактировано название
    изменен readme

--- script/menu.gd
+++ script/menu.gd
@@ -7,10 +7,10 @@ func _process(delta):
 	pass
 
 func _on_singleplayer_pressed():
-	pass
+	get_tree().change_scene_to_file("res://scene/singleplayer.tscn")
 
 func _on_multiplayer_pressed():
-	pass
+	get_tree().change_scene_to_file("res://scene/multiplayer.tscn")
 
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://scene/settings.tscn")
