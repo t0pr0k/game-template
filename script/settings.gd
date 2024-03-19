e4616be1933da2c7948c135964cf04965d3d6b64
t0pr0k <84275926+t0pr0k@users.noreply.github.com>
2024-03-19 22:55:24 +0300

    создание локализации
    добавлена локализация
    изменена нумерация версий
    начато создание настроек
    добавлены костыли для теста функций

--- script/settings.gd
+++ script/settings.gd
@@ -8,3 +8,16 @@ func _process(delta):
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scene/menu.tscn")
+
+
+func _on_ru_pressed():
+	TranslationServer.set_locale("ru")
+
+func _on_en_pressed():
+	TranslationServer.set_locale("en")
+
+func _on_on_pressed():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+func _on_of_pressed():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
